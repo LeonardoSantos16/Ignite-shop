98a22c84078221c79b125a918332bc11335a70f1
leonardo <leodsmf@gmail.com>
2024-09-01 20:31:34 -0300

    pagina de produto commit

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,7 +4,7 @@ import { useKeenSlider } from 'keen-slider/react'
 import { GetServerSideProps } from "next"
 import { HomeContainer, Product } from "../styles/pages/home"
 import { stripe } from "../lib/stripe"
-
+import Link from "next/link"
 import 'keen-slider/keen-slider.min.css'
 import Stripe from "stripe"
 
@@ -14,7 +14,7 @@ interface HomeProps {
     id: string
     name: string
     imageUrl: string
-    price: number
+    price: string
   }[]
 }
 export default function Home({ products }: HomeProps) {
@@ -29,14 +29,17 @@ export default function Home({ products }: HomeProps) {
     <HomeContainer ref={sliderRef} className="keen-slider">
       {products.map(product => {
         return (
-          <Product key={product.id} className="keen-slider__slide">
-            <Image src={product.imageUrl} width={520} height={480} alt="" />
-
-            <footer>
-              <strong>{product.name}</strong>
-              <span>{product.price}</span>
-            </footer>
-          </Product>
+          <Link href={`/product/${product.id}`} key={product.id}>
+            <Product
+              className="keen-slider__slide"
+            >
+              <Image src={product.imageUrl} width={520} height={480} alt="" />
+              <footer>
+                <strong>{product.name}</strong>
+                <span>{product.price}</span>
+              </footer>
+            </Product>
+          </Link>
         )
       })}
     </HomeContainer>
